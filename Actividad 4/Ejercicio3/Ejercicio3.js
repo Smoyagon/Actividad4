af359c60249ff548092964057121ef6f0175650d
Smoyagon <smoyagon@estudiante.ibero.edu.co>
2025-04-30 11:46:26 -0500

    Add files via upload

--- Actividad 4/Ejercicio3/Ejercicio3.js
+++ Actividad 4/Ejercicio3/Ejercicio3.js
@@ -1,14 +1,15 @@
 /*Diseñar un programa que muestre el producto de los 10 primeros
 números impares.*/
 
-let n=0
 let producto=1
 let contador=0
+let impar = []
 
 for (let i = 1; contador < 10; i++) {
     if(i%2!=0){
         contador++
         producto*=i
+        impar.push(i)
     }
 }
-console.log(producto)
+console.log(`Los primeros 10 números impares son ${impar} y el producto de estos es ${producto}`)
